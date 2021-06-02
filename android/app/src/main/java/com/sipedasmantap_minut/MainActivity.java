8294717aa0160c82cc47be26f2b8ff4af9f6036d
Pandu Wicaksono <16.9350@stis.ac.id>
2021-06-02 09:57:47 +0800

    initial

---
+++ android/app/src/main/java/com/sipedasmantap_minut/MainActivity.java
@@ -0,0 +1,15 @@
+package com.sipedasmantap_minut;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "sipedasmantap_minut";
+  }
+}
